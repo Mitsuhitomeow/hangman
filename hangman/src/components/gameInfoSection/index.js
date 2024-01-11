6835ec30ea8dea3dc64e93d555ff8aa7f4e2f79c
Alexey Melnikov <cancem343@gmail.com>
2024-01-11 20:40:28 +0300

    feat: add a random question and answer function

--- hangman/src/components/gameInfoSection/index.js
+++ hangman/src/components/gameInfoSection/index.js
@@ -1,23 +1,13 @@
 import styles from './info.module.scss';
-import { answerQuastion } from '../../database.json';
+import createQuestion from './questionsForEach';
 
 export default function initInfoField() {
   const container = document.querySelector('[class*=main__section_game]');
   const infoSection = document.createElement('section');
-  const answerElement = document.createElement('span');
-  const questionElement = document.createElement('p');
-  const guessesElement = document.createElement('p');
 
-  answerElement.className = `${styles.main__info_answer}`;
-  questionElement.className = `${styles.main__info_question}`;
-  guessesElement.className = `${styles.main__info_guesses}`;
-
-  answerElement.textContent = `${answerQuastion[0].answer}`;
-  questionElement.textContent = `Hint: ${answerQuastion[0].quastion}`;
-  guessesElement.textContent = `guesses: 6 / 6`;
+  infoSection.className = `${styles.main__section_info}`;
 
   container.append(infoSection);
-  infoSection.append(answerElement);
-  infoSection.append(questionElement);
-  infoSection.append(guessesElement);
+
+  createQuestion(infoSection);
 }
---
+++ hangman/src/components/gameInfoSection/questionsForEach.js
@@ -0,0 +1,32 @@
+import styles from './info.module.scss';
+import { answerQuastion } from '../../database.json';
+
+const random = Math.floor(Math.random() * answerQuastion.length);
+const randomAnswer = answerQuastion[random].answer;
+const hiddenAnswer = randomAnswer
+  .split('')
+  .map(() => '_')
+  .join('');
+const randomQuestion = answerQuastion[random].question;
+
+export default function createQuestion(infoSection) {
+  const answerElement = document.createElement('span');
+  const questionElement = document.createElement('p');
+  const guessesElement = document.createElement('p');
+
+  const boldWord = document.createElement('span');
+
+  boldWord.className = `${styles.main__info_span}`;
+  answerElement.className = `${styles.main__info_answer}`;
+  questionElement.className = `${styles.main__info_question}`;
+  guessesElement.className = `${styles.main__info_guesses}`;
+
+  answerElement.textContent = `${hiddenAnswer}`;
+  questionElement.textContent = `${randomQuestion}`;
+  guessesElement.textContent = `guesses: 6 / 6`;
+
+  infoSection.append(answerElement);
+  infoSection.append(questionElement);
+  infoSection.append(guessesElement);
+  questionElement.prepend(boldWord);
+}
