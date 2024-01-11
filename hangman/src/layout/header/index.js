b94074788b08512da01cc74941d8500e662c09ad
Alexey Melnikov <cancem343@gmail.com>
2024-01-11 16:15:52 +0300

    feat: add a background color, add a header section

---
+++ hangman/src/layout/header/index.js
@@ -0,0 +1,17 @@
+import styles from './header.module.scss';
+
+export default function Header() {
+  const HEADER = document.createElement('header');
+  const title = document.createElement('h1');
+  const wrapper = document.createElement('div');
+
+  document.body.append(HEADER);
+  HEADER.append(wrapper);
+  wrapper.append(title);
+
+  title.innerText = 'Hagman Game';
+
+  HEADER.className = `${styles.header}`;
+  wrapper.className = `${styles.wrapper}`;
+  title.className = `${styles.title}`;
+}
--- hangman/src/main.js
+++ hangman/src/main.js
@@ -1,4 +1,6 @@
+import Header from './layout/header';
 import Main from './layout/main';
 import './style/main.scss';
 
+Header();
 Main();
