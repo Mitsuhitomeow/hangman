c380e7d151d69320b8faaf9db6e9e381e6b6e905
Alexey Melnikov <cancem343@gmail.com>
2024-01-11 18:06:39 +0300

    refactor: rename a folder

--- hangman/src/main.js
+++ hangman/src/main.js
@@ -1,6 +1,6 @@
 import initFooter from './components/footer';
 import initHeader from './components/header';
-import initGallows from './components/imageGallows';
+import initGallows from './components/imageSection';
 import initMain from './components/main';
 import './style/main.scss';
 
