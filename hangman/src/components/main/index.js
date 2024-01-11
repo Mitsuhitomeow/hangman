8fdb530f33b84f5876285b78f07929d941d0489b
Alexey Melnikov <cancem343@gmail.com>
2024-01-11 18:31:32 +0300

    refactor: update code

--- hangman/src/components/main/index.js
+++ hangman/src/components/main/index.js
@@ -3,10 +3,16 @@ import styles from './main.module.scss';
 export default function initMain() {
   const MAIN = document.createElement('main');
   const wrapper = document.createElement('div');
+  const hangmanGameSection = document.createElement('div');
+  const imageSection = document.createElement('section');
 
   document.body.appendChild(MAIN);
   MAIN.appendChild(wrapper);
+  wrapper.appendChild(imageSection);
+  wrapper.appendChild(hangmanGameSection);
 
   MAIN.className = `main`;
   wrapper.className = `${styles.main__wrapper}`;
+  imageSection.className = `${styles.main__section_gallows}`;
+  hangmanGameSection.className = `${styles.main__section_game}`;
 }
