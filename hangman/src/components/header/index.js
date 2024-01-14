5d2db139ac253a0a6b4c8b07fc5b77f7bcd73735
Alexey Melnikov <cancem343@gmail.com>
2024-01-14 15:06:00 +0300

    feat: add button sound

--- hangman/src/components/header/index.js
+++ hangman/src/components/header/index.js
@@ -1,17 +1,23 @@
 import styles from './header.module.scss';
+import soundOn from '/sound-on.svg'
 
 export default function initHeader() {
   const HEADER = document.createElement('header');
   const title = document.createElement('h1');
   const wrapper = document.createElement('div');
+  const img = document.createElement('img');
+
+  img.setAttribute('src', soundOn)
 
   document.body.append(HEADER);
   HEADER.append(wrapper);
   wrapper.append(title);
+  wrapper.append(img);
 
   title.innerText = 'Hagman Game';
 
   HEADER.className = `header`;
   wrapper.className = `${styles.header__wrapper}`;
   title.className = `${styles.title}`;
+  img.className = `${styles.header__img_sound}`;
 }
