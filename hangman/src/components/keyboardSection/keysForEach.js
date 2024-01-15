6e3b27e42bc1c28c388364c4dc92b8ecd185b6fc
Alexey Melnikov <cancem343@gmail.com>
2024-01-15 16:42:30 +0300

    feat: to make the sound of the click event depend on the size of the screen

--- hangman/src/components/keyboardSection/keysForEach.js
+++ hangman/src/components/keyboardSection/keysForEach.js
@@ -1,19 +1,32 @@
 import styles from './keyboard.module.scss';
-import mp3 from '/musicMouseClick.mp3';
+import clickMp3 from '/musicMouseClick.mp3';
+import pressMp3 from '/musickPressKey.mp3';
 
 export default function createKey(data, keyboard) {
   data.forEach((key) => {
     const button = document.createElement('button');
     const sound = document.querySelector(`[class*=header__img_sound]`);
-    const audio = new Audio(mp3);
+    const audioClick = new Audio(clickMp3);
+    const audioPress = new Audio(pressMp3);
 
     button.textContent = `${key.key}`;
 
     button.addEventListener('click', () => {
+      /**
+       * Добавил звук на ckick в зависимости от размера экрана:
+       * 1) Если на телефоне открыт то звук печатания на клавиатуре.
+       * 2) Если на компьютере, то звук клика мыши.
+       */
+
+      const screenWidth = window.screen.width;
       button.disabled = true;
 
-      if (sound.classList.contains('sound')) {
-        audio.play();
+      if (sound.classList.contains('sound') && screenWidth > 768) {
+        audioClick.play();
+      }
+
+      if (sound.classList.contains('sound') && screenWidth <= 768) {
+        audioPress.play();
       }
     });
 
